50ee6739c3465c7d4bea04c42a1486bf49838c91
bahri-li <bahri.li@hp.com>
2023-03-30 02:35:52 -0400

    Install packages - @react-native-community/slider, react-native-a11y-slider

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import {ScrollView, Text, View, Modal, Button, Switch} from 'react-native';
 import Routes from './src/Routes';
 
 const App = () => {
-  return <Routes />;
+  const onThumbColor = '#ff0000';
+  const offThumbColor = '#00ff00';
+  const [value, onValueChange] = React.useState(true);
+  // return <Routes />;
+  return (<Switch
+    thumbColor={value ? onThumbColor : offThumbColor}
+    value={value}
+    onValueChange={(val: boolean) => {
+      onValueChange(val);
+    }}
+  />);
 };
 
 export default App;
